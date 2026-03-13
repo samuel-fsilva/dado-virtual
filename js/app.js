5f2fc2384732d25d8b1038f66fc9f6dceb32044c
samuel.fs <samuel.dasilva1211@gmail.com>
2026-03-13 10:42:05 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,38 +2,71 @@ const diceBox = document.getElementById("dice-box");
 const quantDicesInput = document.getElementById("dice-quantity");
 let diceQuantity = 1;
 const submitButton = document.getElementById("submit");
+const quantLimit = 27;
+const diceAudio = new Audio("audio/dice-142528.mp3");
 
 window.addEventListener("DOMContentLoaded", () => {
-  for (let i = diceQuantity; i > 0; i--) {
-    let dice = document.createElement("div");
-    dice.className = "dice";
-    diceBox.appendChild(dice);
+  changeDiceQuant(diceQuantity);
+  quantDicesInput.value = diceQuantity;
+});
+
+window.addEventListener("resize", () => {
+  if (diceQuantity > 1) {
+    fixDiceBox(diceQuantity);
   }
+});
+
+submitButton.addEventListener("click", (event) => {
+  lancar();
 });
 
 quantDicesInput.addEventListener("change", () => {
   diceQuantity = quantDicesInput.value;
-  chageDiceQuant(diceQuantity);
+  changeDiceQuant(diceQuantity);
 });
 
-function chageDiceQuant(dq) {
-  diceBox.innerHTML = "";
-  for (let i = dq; i > 0; i--) {
-    let dice = document.createElement("div");
-    dice.className = "dice";
-    diceBox.appendChild(dice);
-  }
-  if (dq > 1) {
-    submitButton.innerText = "Lançar os Dados!";
-    submitButton.disabled = false;
-    // diceBox.style.cssText = `${}`
-  } else if (dq == 1) {
-    submitButton.innerText = "Lançar o Dado!";
-    submitButton.disabled = false;
-  } else {
-    submitButton.disabled = true;
+function changeDiceQuant(dq) {
+  if (dq >= 1 && dq <= quantLimit) {
+    diceBox.innerHTML = "";
+    for (let i = dq; i > 0; i--) {
+      let dice = document.createElement("div");
+      dice.className = "dice";
+      diceBox.appendChild(dice);
+      dice.classList.add(`bg0${Math.floor(Math.random() * 6) + 1}`);
+    }
+
+    if (dq > 1) {
+      submitButton.innerText = "Lançar os Dados!";
+      submitButton.disabled = false;
+      fixDiceBox(diceQuantity);
+    } else if (dq == 1) {
+      submitButton.innerText = "Lançar o Dado!";
+      submitButton.disabled = false;
+      diceBox.style.cssText = "";
+    } else {
+      submitButton.disabled = true;
+    }
+  } else if (dq < 1) {
+    quantDicesInput.value = 1;
+    changeDiceQuant(1);
+  } else if (dq > quantLimit) {
+    quantDicesInput.value = quantLimit;
   }
 }
+function fixDiceBox(dq) {
+  let scaleFactor = dq ** 2 / ((dq ** 2 - dq) * 2);
+  diceBox.style.cssText = `scale: ${scaleFactor};
+     width: ${parseFloat(window.getComputedStyle(document.body).width) * (1 / scaleFactor)}px`;
+}
+function dicePlayAudio() {
+  diceAudio.currentTime = 0;
+  diceAudio.play();
+}
+function lancar(event) {
+  event.target.disabled = true;
+  dicePlayAudio();
+}
+
 /*window.addEventListener("load", () => {
   randomDiceFace();
 });
